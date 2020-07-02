a156ea65506d6755c42943c6c26918087964d235
mrglnt <63204066+mrglnt@users.noreply.github.com>
2020-07-02 02:54:27 +0200

    web scrapper added, __main__ added

---
+++ __main__.py
@@ -0,0 +1,14 @@
+from web_scrapper import Parser
+
+
+def main():
+    # preprocessing
+    par = Parser()
+    par.load_url()
+    par.fit()
+
+    # processing
+
+
+if __name__ == '__main__':
+    main()
